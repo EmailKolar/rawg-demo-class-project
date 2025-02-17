5b229ae5008683a7e7066eb8b4fc4b1c14a9fd64
EmailKolar <Emilaalund.nyboder@gmail.com>
2025-02-17 10:49:51 +0100

    changes

---
+++ rawg-client/src/components/PlatformIconsList.tsx
@@ -0,0 +1,55 @@
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconsList = ({ platforms }: Props) => {
+  const getIcon = (slug: string) => {
+    switch (slug) {
+      case "pc":
+        return FaWindows;
+      case "playstation":
+        return FaPlaystation;
+      case "xbox":
+        return FaXbox;
+      case "mac":
+        return FaApple;
+      case "linux":
+        return FaLinux;
+      case "android":
+        return FaAndroid;
+      case "ios":
+        return MdPhoneIphone;
+      case "nintendo":
+        return SiNintendo;
+      default:
+        return null; // Return null for unknown platforms
+    }
+  };
+
+  return (
+    <HStack margin={1}>
+      {platforms.map((platform) => {
+        const IconComponent = getIcon(platform.slug);
+        return IconComponent ? (
+          <Icon key={platform.id} as={IconComponent} boxSize={5} />
+        ) : null;
+      })}
+    </HStack>
+  );
+};
+
+export default PlatformIconsList;
