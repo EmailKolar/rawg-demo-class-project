7ae90e3d88326cf51820df5bafcd84611d2c8ff1
EmailKolar <Emilaalund.nyboder@gmail.com>
2025-02-24 08:50:10 +0100

    add GenreList component to display genres and update App layout - also added tsconfig for testing

--- rawg-client/src/App.tsx
+++ rawg-client/src/App.tsx
@@ -2,6 +2,7 @@
 import { Grid, GridItem, Show } from '@chakra-ui/react'
 import NavBar from './components/NavBar'
 import GameGrid from './components/GameGrid'
+import GenreList from './components/GenreList'
 
 
 function App() {
@@ -19,7 +20,7 @@ function App() {
     </GridItem>
     <Show above="lg">
     <GridItem pl='2' bg='pink.300' area={'aside'}>
-      Aside
+     <GenreList/>
     </GridItem>
     </Show>
     <GridItem pl='2' bg='green.300' area={'main'}>
--- rawg-client/src/components/GenreList.tsx
+++ rawg-client/src/components/GenreList.tsx
@@ -5,9 +5,17 @@ import useGenres from '../hooks/useGenres'
 const GenreList = () => {
 const {genres, error, isLoading} = useGenres();
 
-  return (
-    <div>GenreList</div>
-  )
-}
+return (
+  <div>
+    {error && <p>{error}</p>}
+    {isLoading && <p>Loading...</p>}
+    <ul>
+      {genres.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  </div>
+);
+};
 
-export default GenreList
+export default GenreList;
