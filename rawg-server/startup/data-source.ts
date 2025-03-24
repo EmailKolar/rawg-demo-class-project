771fbd544f8a231e5c7b9ae329221d7214fc3811
emailkolar <emilaalund.nyboder@gmail.com>
2025-03-24 11:27:09 +0100

    feat: add rating, released, and added fields to Game entity; implement ordering functionality in gameRouter

--- rawg-server/startup/data-source.ts
+++ rawg-server/startup/data-source.ts
@@ -1,30 +1,17 @@
+import "dotenv/config";
 import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Game } from "../entities/Game";
 import { Genre } from "../entities/Genre";
 import { Store } from "../entities/Store";
 import { ParentPlatform } from "../entities/ParentPlatform";
-import "dotenv/config";
-
-const connectionString = process.env.MYSQL_URL;
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  url: connectionString,
+  url: process.env.DATABASE_URL,
   synchronize: false, // Set to false in production and use migrations instead
   logging: true,
   entities: [Game, Genre, Store, ParentPlatform],
   migrations: [],
   subscribers: [],
 });
-
-/*
-// Initialize the data source
-AppDataSource.initialize()
-  .then(() => {
-    console.log("Data Source has been initialized!");
-  })
-  .catch((err) => {
-    console.error("Error during Data Source initialization:", err);
-  });
-*/
