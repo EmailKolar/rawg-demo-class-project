6ab22127ad57fae7636f8092092bc1880eb580ad
emailkolar <emilaalund.nyboder@gmail.com>
2025-04-28 10:31:04 +0200

    hover and get descriptopn from seeder

--- rawg-client/src/domain/game/GameCardContainer.tsx
+++ rawg-client/src/domain/game/GameCardContainer.tsx
@@ -1,16 +1,20 @@
-import { Box } from '@chakra-ui/react';
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import { ReactNode } from "react";
 
-interface Props {  
-    children: React.ReactNode;
+interface Props {
+  children: ReactNode;
 }
 
-const GameCardContainer = ({children}: Props) => {
+const GameCardContainer = ({ children }: Props) => {
   return (
-   <Box overflow="hidden" borderRadius={10}>
-    {children}
+    <Box
+      _hover={{ transform: "scale(1.05)", transition: "0.2s" }}
+      overflow="hidden"
+      borderRadius={10}
+    >
+      {children}
     </Box>
-  )
-}
+  );
+};
 
-export default GameCardContainer
+export default GameCardContainer;
